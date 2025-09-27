8e871aa03e1a77fcb7816ed732b4a3773ac73ee0
Jenna Fligor <jenna@fligor.net>
2025-09-26 20:16:32 -0500

    immich migration 1/2

--- memory-hole/immich/default.nix
+++ memory-hole/immich/default.nix
@@ -5,21 +5,51 @@
   nixpkgs,
   ...
 }: {
-  services.immich = {
-    enable = true;
-    database.enable = true;
-    host = "0.0.0.0";
-    port = 2283;
-    openFirewall = true;
-    # accelerationDevices = null;
-    mediaLocation = "/mnt/immich";
-  };
+  containers.immich = let
+    dbDir = "/mnt/postgres";
+  in {
+    autoStart = true;
+    ephemeral = true;
+    bindMounts = {
+      immich-db = {
+        mountPoint = dbDir;
+        hostPath = "/mnt/immich-db";
+        isReadOnly = false;
+      };
+      syncthing = rec {
+        mountPoint = "/mnt/syncthing";
+        hostPath = mountPoint;
+        isReadOnly = false;
+      };
+    };
+    config = let
+      globalConfig = config;
+    in
+      {
+        config,
+        pkgs,
+        lib,
+        ...
+      }: {
+        services.immich = {
+          enable = true;
+          database.enable = true;
+          host = "0.0.0.0";
+          port = 2283;
+          openFirewall = true;
+          # accelerationDevices = null;
+          mediaLocation = "/var/empty";
+        };
 
-  users.users.immich.extraGroups = ["video" "render"];
+        users.users.immich.extraGroups = ["video" "render"];
 
-  services.postgresql = {
-    dataDir = "/mnt/immich-postgres";
-    settings.port = 5432;
+        services.postgresql = {
+          dataDir = dbDir;
+          settings.port = 5432;
+        };
+
+        system.stateVersion = globalConfig.system.stateVersion;
+      };
   };
 
   environment.systemPackages = [pkgs.immich-cli];
