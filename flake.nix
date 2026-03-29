32939cd5938b1f6889df983aebe25f887cbec41a
Jenna Fligor <jenna@fligor.net>
2026-03-28 21:51:29 -0500

    bump to 25.11 (better late than never)

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     sops-nix = {
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
